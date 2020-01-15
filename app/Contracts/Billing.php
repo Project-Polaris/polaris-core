c6de4abd16d9a87a5cf033c66ae34281bfcf72a5
CoreyAndres <56902932+CoreyAndres@users.noreply.github.com>
2020-01-15 16:35:13 +0800

    add contracts

--- app/Contracts/Billing.php
+++ app/Contracts/Billing.php
@@ -1,14 +1,12 @@
 <?php
 
-namespace App\Interfaces;
+namespace App\Contracts;
 
 use App\Models\User;
 use App\Models\Server;
 use App\Models\Prototypes\UserBillingStatus;
-use App\Interfaces\IProtocol;
 
-interface IBilling {
-    public static function getInstance(): IBilling;
+interface Billing {
     public function getUserStatus(User $user): ?array;
     public function checkUserProtocolAccess(User $user, Server $server, IProtocol $protocol): bool;
 }
